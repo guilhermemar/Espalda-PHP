41ddb9bfa0556cdeeb62cae736aae5111a9954c2
vagrant <vagrant@precise32.(none)>
2014-06-26 22:57:22 -0300

    using angular

---
+++ docs/manual/js/controllers/menu.js
@@ -0,0 +1,12 @@
+function controllerMenu ($scope)
+{
+	$scope.menu = [
+		{
+			groupName : "",
+			itens : [
+			 {name : "About", link : "about"},
+			 {name : "Outro", link : "outro"}
+			]
+		}
+	];
+}
