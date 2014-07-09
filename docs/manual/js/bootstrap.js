18e6ca98313d9ea03dd43094301a0a252cf071dd
vagrant <vagrant@precise32.(none)>
2014-07-09 19:58:22 -0300

    b

--- docs/manual/js/bootstrap.js
+++ docs/manual/js/bootstrap.js
@@ -2,16 +2,10 @@ window.app = angular.module('documentation', []);
 
 window.app.factory('loadContent', function loadContent ($http) {
 	
-	return function loadContent (content, success, error) {
-		$http.get('content/' + content + '.json').success(success).error(error);
+	return function loadContent (content, success) {
+		$http.get('content/' + content + '.json').success(success).error(function(){
+			success([{type : 'title', content : 'There is a problem. Sorry!'}]);
+		});
 	}
 	
 });
-/*
-window.app.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.
-			when('/about', {templateUrl: 'about.html'}).
-			when('/outro', {template: 'OUTRO'}).
-			otherwise({redirectTo: '/about'});
-}]);
-*/
--- docs/manual/js/controllers/content.js
+++ docs/manual/js/controllers/content.js
@@ -7,12 +7,8 @@ window.app.controller('controllerContent', function controllerContent ($scope, $
 		if (newContent) {
 			
 			loadContent(newContent, function (data) {
-				
+				console.log(data);
 				$scope.allContent = data;
-				
-			}, function (data) {
-				
-				console.log('deu pau no loading');
 				
 			});
 
