6d27d24b7e5394dbb91fd272b7e5391b81f86f92
Mar, Guilherme <gfmarster@gmail.com>
2014-08-02 15:31:27 -0300

    avant

--- docs/manual/js/controllers/content.js
+++ docs/manual/js/controllers/content.js
@@ -7,12 +7,29 @@ window.app.controller('controllerContent', function controllerContent ($scope, $
 		if (newContent) {
 			
 			loadContent(newContent, function (data) {
-				console.log(data);
 				$scope.allContent = data;
 				
 			});
-
+			
+			
 		}
 	});
 	
+});
+
+window.app.controller('controllerLoadGist', function controllerLoadGist ($scope, $element) {
+	
+	//console.log($element);
+	//console.log($scope);
+	
+	$scope.init = function (b) { console.log(b)};
+
+	
+	//var s = document.createElement('script');
+	
+	//$element.append(s);
+	//$element.context.innerHTML = 'bla bla bla';
+	
+	//console.log($element.context.attributes.someting.textContent);
+	//return;
 });
