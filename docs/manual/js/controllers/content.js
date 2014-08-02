cba40d5bec7219c756fa911029e8539717fef4b1
Mar, Guilherme <gfmarster@gmail.com>
2014-08-02 17:07:42 -0300

    a alternative

--- docs/manual/js/controllers/content.js
+++ docs/manual/js/controllers/content.js
@@ -19,10 +19,29 @@ window.app.controller('controllerContent', function controllerContent ($scope, $
 
 window.app.controller('controllerLoadGist', function controllerLoadGist ($scope, $element) {
 	
+	
+	
+	setTimeout(function(){
+		//alert($element.context.attributes.gistUrl.value);
+		var s = document.createElement('script');
+		s.src = $element.context.attributes.gistUrl.value;
+		//$element.append(s);
+		
+		console.log($scope);
+		
+		b = $element.context;
+		console.log($element);
+		
+		b.appendChild(s);
+		
+		//document.write('<script src="https://gist.github.com/guilhermemar/9a82cb98d787278f3d3d.js"></script>');
+		
+	}, 500);
+		
 	//console.log($element);
 	//console.log($scope);
 	
-	$scope.init = function (b) { console.log(b)};
+	//$scope.init = function (b) { console.log(b)};
 
 	
 	//var s = document.createElement('script');
